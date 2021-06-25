861943d452a157d0328d3dcb22ddc2c170967ade
summerji <summerji@google.com>
2021-06-24 18:00:46 -0700

    feat: add protobufjs int64 conversion

--- proto/generate.sh
+++ proto/generate.sh
@@ -4,3 +4,4 @@ protoc -I . --python_out ../python intjson.proto
 protoc -I . --java_out ../java/src/main/java intjson.proto
 protoc -I . --ruby_out ../ruby intjson.proto
 protoc -I . --csharp_out ../csharp intjson.proto
+npx pbjs -t json -o ../nodejs/intjson.json intjson.proto
